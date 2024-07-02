ef68b6e3a6714ffc0cbf4e71e255c07c42cd8f35
Samdevtechnology <Samdevtechnology@gmail.com>
2024-07-02 12:28:25 +0100

    Testing - Ip Address

--- controllers/hello.js
+++ controllers/hello.js
@@ -10,39 +10,41 @@ export const getHello = async (req, res, next) => {
   const { visitor_name } = req.query;
   const ip = req.ip;
 
-  try {
-    // Fetch geolocation data
-    const ipinfoResponse = await axios.get(
-      `https://ipinfo.io/${ip}?token=${ipinfoToken}`
-    );
-    const locationData = ipinfoResponse.data;
-    const city = locationData.city;
+  res.send(`Your current Ip address is IP address id ${ip}`);
 
-    if (!city) {
-      return res.send("City information not available");
-    }
+  // try {
+  //   // Fetch geolocation data
+  //   const ipinfoResponse = await axios.get(
+  //     `https://ipinfo.io/${ip}?token=${ipinfoToken}`
+  //   );
+  //   const locationData = ipinfoResponse.data;
+  //   const city = locationData.city;
 
-    // Fetch weather data
-    const weatherResponse = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather`,
-      {
-        params: {
-          q: "lagos",
-          appid: openWeatherMapApiKey,
-          units: "metric",
-        },
-      }
-    );
-    const weatherData = weatherResponse.data;
-    const temp = weatherData.main.temp;
+  //   if (!city) {
+  //     return res.send("City information not available");
+  //   }
 
-    res.json({
-      client_ip: req.ip, // The IP address of the requester
-      location: `${city}`, // The city of the requester
-      greeting: `Hello, ${visitor_name}!, the temperature is ${temp} degrees Celsius in ${city}`,
-    });
-  } catch (error) {
-    console.error("Error fetching geolocation or weather data:", error);
-    res.status(500).send("Error fetching geolocation or weather data");
-  }
+  //   // Fetch weather data
+  //   const weatherResponse = await axios.get(
+  //     `https://api.openweathermap.org/data/2.5/weather`,
+  //     {
+  //       params: {
+  //         q: "lagos",
+  //         appid: openWeatherMapApiKey,
+  //         units: "metric",
+  //       },
+  //     }
+  //   );
+  //   const weatherData = weatherResponse.data;
+  //   const temp = weatherData.main.temp;
+
+  //   res.json({
+  //     client_ip: req.ip, // The IP address of the requester
+  //     location: `${city}`, // The city of the requester
+  //     greeting: `Hello, ${visitor_name}!, the temperature is ${temp} degrees Celsius in ${city}`,
+  //   });
+  // } catch (error) {
+  //   console.error("Error fetching geolocation or weather data:", error);
+  //   res.status(500).send("Error fetching geolocation or weather data");
+  // }
 };
