176d40e71d3477a84c85835d6743c20afd774c50
Samdevtechnology <Samdevtechnology@gmail.com>
2024-07-02 16:10:14 +0100

    Added - Styles and Template

--- controllers/hello.js
+++ controllers/hello.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 import dotenv from "dotenv";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 dotenv.config();
 
@@ -7,53 +12,55 @@ const ipinfoToken = process.env.IPINFO_TOKEN;
 const openWeatherMapApiKey = process.env.OPENWEATHERMAP_API_KEY;
 
 export const getHello = async (req, res, next) => {
-  const { visitor_name } = req.query;
-  const ip =
-    req.headers["x-forwarded-for"] || req.socket.remoteAddress || req.ip;
+  let { visitor_name: visitorName = "Visitor" } = req.query;
+  const ip = "102.89.22.221";
+  // req.headers["x-forwarded-for"] || req.socket.remoteAddress || req.ip;
 
-  const ipinfoResponse = await axios.get(
-    `https://ipinfo.io/${ip}?token=${ipinfoToken}`
-  );
-  const locationData = ipinfoResponse.data;
-  console.log("🚀 ~ getHello ~ locationData:", locationData);
-  const city = locationData.city;
-  console.log("🚀 ~ getHello ~ city:", city);
+  // Remove quotes from visitor_name
+  visitorName = visitorName.replace(/['"]+/g, "");
 
-  res.send(`Your current Ip address is IP address id ${ip}`);
+  try {
+    // Fetching geolocation data
+    const ipinfoResponse = await axios.get(
+      `https://ipinfo.io/${ip}?token=${ipinfoToken}`
+    );
+    const locationData = ipinfoResponse.data;
+    const city = locationData.city;
 
-  // try {
-  //   // Fetch geolocation data
-  //   const ipinfoResponse = await axios.get(
-  //     `https://ipinfo.io/${ip}?token=${ipinfoToken}`
-  //   );
-  //   const locationData = ipinfoResponse.data;
-  //   const city = locationData.city;
+    if (!city) {
+      return res.send("City information not available");
+    }
 
-  //   if (!city) {
-  //     return res.send("City information not available");
-  //   }
+    // Fetching weather data (for temperature)
+    const weatherResponse = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather`,
+      {
+        params: {
+          q: "lagos",
+          appid: openWeatherMapApiKey,
+          units: "metric",
+        },
+      }
+    );
+    const weatherData = weatherResponse.data;
+    const temp = weatherData.main.temp;
 
-  //   // Fetch weather data
-  //   const weatherResponse = await axios.get(
-  //     `https://api.openweathermap.org/data/2.5/weather`,
-  //     {
-  //       params: {
-  //         q: "lagos",
-  //         appid: openWeatherMapApiKey,
-  //         units: "metric",
-  //       },
-  //     }
-  //   );
-  //   const weatherData = weatherResponse.data;
-  //   const temp = weatherData.main.temp;
-
-  //   res.json({
-  //     client_ip: req.ip, // The IP address of the requester
-  //     location: `${city}`, // The city of the requester
-  //     greeting: `Hello, ${visitor_name}!, the temperature is ${temp} degrees Celsius in ${city}`,
-  //   });
-  // } catch (error) {
-  //   console.error("Error fetching geolocation or weather data:", error);
-  //   res.status(500).send("Error fetching geolocation or weather data");
-  // }
+    if (req.accepts("html")) {
+      res.render("hello", {
+        ip,
+        visitorName,
+        city,
+        temp,
+      });
+    } else {
+      res.json({
+        client_ip: ip, // The IP address of the requester
+        location: `${city}`, // The city of the requester
+        greeting: `Hello, ${visitorName}!, the temperature is ${temp} degrees Celsius in ${city}`,
+      });
+    }
+  } catch (error) {
+    console.error("Error fetching geolocation or weather data:", error);
+    res.status(500).send("Error fetching geolocation or weather data");
+  }
 };
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 import express from "express";
-import path from "path";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import hello from "./routes/hello.js";
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 //Routes
 app.use("/api/hello", hello);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.all("*", (req, res) => {
+  res.status(404);
+  res.render("404");
+});
 
 app.listen(port, () => console.log(`server is up and running on port ${port}`));
